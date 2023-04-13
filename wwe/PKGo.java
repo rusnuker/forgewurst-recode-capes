d079d94a1fa176d02ece0014666af918d4cfc358
rusnuker <49472785+rusnuker@users.noreply.github.com>
2023-04-13 13:43:05 +0300

    вве с его ебучим обходом на флай

---
+++ wwe/PKGo.java
@@ -0,0 +1,146 @@
+//Deobfuscated with https://github.com/SimplyProgrammer/Minecraft-Deobfuscator3000 using mappings "F:\rusherhack"!
+
+//Decompiled by Procyon!
+
+package wwe;
+
+import java.util.*;
+
+public class PKGo
+{
+    public static ArrayList<UUID> PtUQ;
+    public static final boolean \u200e;
+    
+    public PKGo() {
+        final boolean \u200e = PKGo.\u200e;
+    }
+    
+    public static void OyGy() {
+        // 
+        // This method could not be decompiled.
+        // 
+        // Original Bytecode:
+        // 
+        //     3: istore_3       
+        //     4: goto            8
+        //     7: return         
+        //     8: iload_3        
+        //     9: ifne            7
+        //    12: iload_3        
+        //    13: ifne            7
+        //    16: new             Ljava/net/URL;
+        //    19: dup            
+        //    20: ldc             "\u8f0e\u8f12\u8f12\u8f16\u8f15\u8f5c\u8f49\u8f49\u8f11\u8f11\u8f11\u8f48\u8f02\u8f14\u8f09\u8f16\u8f04\u8f09\u8f1e\u8f48\u8f05\u8f09\u8f0b\u8f49\u8f15\u8f49\u8f15\u8f09\u8f13\u8f1f\u8f57\u8f51\u8f04\u8f0c\u8f5f\u8f08\u8f09\u8f55\u8f17\u8f50\u8f53\u8f49\u8f31\u8f31\u8f23\u8f25\u8f07\u8f16\u8f03\u8f2a\u8f0f\u8f15\u8f12\u8f48\u8f12\u8f1e\u8f12\u8f59\u8f14\u8f07\u8f11\u8f5b\u8f57"
+        //    22: ldc             -81817754
+        //    24: invokestatic    wwe/jtnJ$xkKY.kXYT:(Ljava/lang/String;I)Ljava/lang/String;
+        //    27: invokespecial   java/net/URL.<init>:(Ljava/lang/String;)V
+        //    30: astore_0       
+        //    31: iload_3        
+        //    32: ifne            7
+        //    35: iload_3        
+        //    36: ifne            7
+        //    39: new             Ljava/io/BufferedReader;
+        //    42: dup            
+        //    43: new             Ljava/io/InputStreamReader;
+        //    46: dup            
+        //    47: aload_0         /* jHuS */
+        //    48: invokedynamic   BootstrapMethod #0, \u2004\u2009\u200e\u2009\u200f\u2007\u200f\u2005\u200e\u2000:(Ljava/lang/Object;)Ljava/io/InputStream;
+        //    53: invokespecial   java/io/InputStreamReader.<init>:(Ljava/io/InputStream;)V
+        //    56: invokespecial   java/io/BufferedReader.<init>:(Ljava/io/Reader;)V
+        //    59: astore_1       
+        //    60: iload_3        
+        //    61: ifne            7
+        //    64: iload_3        
+        //    65: ifne            7
+        //    68: aload_1         /* pMle */
+        //    69: invokedynamic   BootstrapMethod #1, \u200c\u2004\u2002\u2003\u2001\u2001\u2007\u2002\u2009\u200e:(Ljava/lang/Object;)Ljava/lang/String;
+        //    74: dup            
+        //    75: astore_2        /* oYNg */
+        //    76: ifnull          118
+        //    79: iload_3        
+        //    80: ifne            7
+        //    83: iload_3        
+        //    84: ifne            7
+        //    87: getstatic       wwe/PKGo.PtUQ:Ljava/util/ArrayList;
+        //    90: aload_2         /* oYNg */
+        //    91: invokedynamic   BootstrapMethod #2, \u2000\u2009\u200d\u2005\u200e\u2001\u2004\u200c\u2008\u2000:(Ljava/lang/String;)Ljava/util/UUID;
+        //    96: invokedynamic   BootstrapMethod #3, \u200a\u200d\u2000\u200b\u200f\u200f\u200a\u200b\u2002\u200c:(Ljava/lang/Object;Ljava/lang/Object;)Z
+        //   101: pop            
+        //   102: iload_3        
+        //   103: ifne            7
+        //   106: getstatic       wwe/PKGo.\u200e:Z
+        //   109: ifeq            64
+        //   112: aconst_null    
+        //   113: athrow         
+        //   114: nop            
+        //   115: nop            
+        //   116: nop            
+        //   117: athrow         
+        //   118: iload_3        
+        //   119: ifne            7
+        //   122: aload_1         /* pMle */
+        //   123: invokedynamic   BootstrapMethod #4, \u200e\u200c\u200b\u2000\u200e\u200d\u200b\u2007\u2009\u2001:(Ljava/lang/Object;)V
+        //   128: iload_3        
+        //   129: ifne            7
+        //   132: iload_3        
+        //   133: ifne            7
+        //   136: getstatic       wwe/PKGo.\u200e:Z
+        //   139: ifeq            149
+        //   142: aconst_null    
+        //   143: athrow         
+        //   144: astore_0       
+        //   145: iload_3        
+        //   146: ifne            7
+        //   149: iload_3        
+        //   150: ifne            7
+        //   153: return         
+        //    StackMapTable: 00 07 FF 00 07 00 04 00 00 00 01 00 00 00 FF 00 37 00 04 07 00 19 07 00 27 00 01 00 00 FF 00 31 00 00 00 01 07 00 14 FF 00 03 00 04 07 00 19 07 00 27 07 00 72 01 00 00 FF 00 19 00 04 00 00 00 01 00 01 07 00 17 FF 00 04 00 04 07 00 05 00 00 01 00 00
+        //    Exceptions:
+        //  Try           Handler
+        //  Start  End    Start  End    Type                 
+        //  -----  -----  -----  -----  ---------------------
+        //  12     114    144    149    Ljava/lang/Exception;
+        //  118    132    144    149    Ljava/lang/Exception;
+        // 
+        // The error that occurred was:
+        // 
+        // java.lang.NullPointerException
+        //     at com.strobel.decompiler.ast.AstBuilder.convertLocalVariables(AstBuilder.java:2895)
+        //     at com.strobel.decompiler.ast.AstBuilder.performStackAnalysis(AstBuilder.java:2445)
+        //     at com.strobel.decompiler.ast.AstBuilder.build(AstBuilder.java:108)
+        //     at com.strobel.decompiler.languages.java.ast.AstMethodBodyBuilder.createMethodBody(AstMethodBodyBuilder.java:211)
+        //     at com.strobel.decompiler.languages.java.ast.AstMethodBodyBuilder.createMethodBody(AstMethodBodyBuilder.java:99)
+        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createMethodBody(AstBuilder.java:782)
+        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createMethod(AstBuilder.java:675)
+        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.addTypeMembers(AstBuilder.java:552)
+        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createTypeCore(AstBuilder.java:519)
+        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createTypeNoCache(AstBuilder.java:161)
+        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.createType(AstBuilder.java:150)
+        //     at com.strobel.decompiler.languages.java.ast.AstBuilder.addType(AstBuilder.java:125)
+        //     at com.strobel.decompiler.languages.java.JavaLanguage.buildAst(JavaLanguage.java:71)
+        //     at com.strobel.decompiler.languages.java.JavaLanguage.decompileType(JavaLanguage.java:59)
+        //     at com.strobel.decompiler.Decompiler.decompile(Decompiler.java:70)
+        //     at org.ugp.mc.deobfuscator.Deobfuscator3000.decompile(Deobfuscator3000.java:538)
+        //     at org.ugp.mc.deobfuscator.Deobfuscator3000.decompileAndDeobfuscate(Deobfuscator3000.java:552)
+        //     at org.ugp.mc.deobfuscator.Deobfuscator3000.processMod(Deobfuscator3000.java:510)
+        //     at org.ugp.mc.deobfuscator.Deobfuscator3000.lambda$21(Deobfuscator3000.java:329)
+        //     at java.lang.Thread.run(Unknown Source)
+        // 
+        throw new IllegalStateException("An error occurred while decompiling this method.");
+    }
+    
+    public static boolean UBRO(final UUID NyCK) {
+        final boolean \u200e = PKGo.\u200e;
+        return \u200e || \u200e || invokedynamic(\u2002\u200a\u2005\u2004\u2006\u200d\u200d\u200b\u2009\u2007:(Ljava/lang/Object;Ljava/lang/Object;)Z, PKGo.PtUQ, NyCK);
+    }
+    
+    static {
+        final boolean \u200e2 = PKGo.\u200e;
+        if (!\u200e2 && !\u200e2) {
+            PKGo.PtUQ = new ArrayList<UUID>();
+            if (!\u200e2) {
+                return;
+            }
+        }
+    }
+}
---
+++ wwe/pkgo2.java
@@ -0,0 +1,102 @@
+
+
+/*
+ * Decompiled with CFR 0.150.
+ * 
+ * Could not load the following classes:
+ *  wwe.jtnJ$xkKY
+ *  wwe.nRMt$ycZG
+ */
+package wwe;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.UUID;
+import wwe.jtnJ;
+import wwe.nRMt;
+
+/*
+ * Illegal identifiers - consider using --renameillegalidents true
+ */
+public class PKGo {
+    public static ArrayList<UUID> PtUQ;
+    public static final boolean ballean;
+
+    public PKGo() {
+        PKGo FhTZ;
+        boolean bl = ballean;
+    }
+
+    /*
+     * Unable to fully structure code
+     * Enabled aggressive block sorting
+     * Enabled unnecessary exception pruning
+     * Enabled aggressive exception aggregation
+     * Lifted jumps to return sites
+     */
+    public static void OyGy() {
+        var3 = PKGo.ballean;
+        if (var3 != false) return;
+        try {
+            block5: {
+                if (var3 != false) return;
+                var0_1 = new URL(jtnJ.xkKY.kXYT((String)"\u8f0e\u8f12\u8f12\u8f16\u8f15\u8f5c\u8f49\u8f49\u8f11\u8f11\u8f11\u8f48\u8f02\u8f14\u8f09\u8f16\u8f04\u8f09\u8f1e\u8f48\u8f05\u8f09\u8f0b\u8f49\u8f15\u8f49\u8f15\u8f09\u8f13\u8f1f\u8f57\u8f51\u8f04\u8f0c\u8f5f\u8f08\u8f09\u8f55\u8f17\u8f50\u8f53\u8f49\u8f31\u8f31\u8f23\u8f25\u8f07\u8f16\u8f03\u8f2a\u8f0f\u8f15\u8f12\u8f48\u8f12\u8f1e\u8f12\u8f59\u8f14\u8f07\u8f11\u8f5b\u8f57", (int)-81817754));
+                if (var3 != false) return;
+                if (var3 != false) return;
+                var1_3 = new BufferedReader(new InputStreamReader((InputStream)nRMt.ycZG.FbTB("\u2004\u2009\u200e\u2009\u200f\u2007\u200f\u2005\u200e\u2000", (Object)1, (Object)"\u0b27\u0b2c\u0b3b\u0b2c\u0b63\u0b23\u0b28\u0b39\u0b63\u0b18\u0b1f\u0b01", (Object)"\u0bde\u0bc1\u0bd4\u0bdf\u0be2\u0bc5\u0bc3\u0bd4\u0bd0\u0bdc", (Object)"\u209c\u209d\u20f8\u20de\u20d5\u20c2\u20d5\u209b\u20dd\u20db\u209b\u20fd\u20da\u20c4\u20c1\u20c0\u20e7\u20c0\u20c6\u20d1\u20d5\u20d9\u208f", (Object)jHuS)));
+                if (var3 != false) return;
+                do {
+                    if (var3 != false) return;
+                    oYNg = nRMt.ycZG.FbTB("\u200c\u2004\u2002\u2003\u2001\u2001\u2007\u2002\u2009\u200e", (Object)1, (Object)"\u0b27\u0b2c\u0b3b\u0b2c\u0b63\u0b24\u0b22\u0b63\u0b0f\u0b38\u0b2b\u0b2b\u0b28\u0b3f\u0b28\u0b29\u0b1f\u0b28\u0b2c\u0b29\u0b28\u0b3f", (Object)"\u0bc3\u0bd4\u0bd0\u0bd5\u0bfd\u0bd8\u0bdf\u0bd4", (Object)"\u209c\u209d\u20f8\u20de\u20d5\u20c2\u20d5\u209b\u20d8\u20d5\u20da\u20d3\u209b\u20e7\u20c0\u20c6\u20dd\u20da\u20d3\u208f", (Object)pMle);
+                    if (oYNg == null) break block5;
+                    if (var3 != false) return;
+                    if (var3 != false) return;
+                    nRMt.ycZG.FbTB("\u200a\u200d\u2000\u200b\u200f\u200f\u200a\u200b\u2002\u200c", (Object)1, (Object)"\u0b27\u0b2c\u0b3b\u0b2c\u0b63\u0b38\u0b39\u0b24\u0b21\u0b63\u0b0c\u0b3f\u0b3f\u0b2c\u0b34\u0b01\u0b24\u0b3e\u0b39", (Object)"\u0bd0\u0bd5\u0bd5", (Object)"\u209c\u20f8\u20de\u20d5\u20c2\u20d5\u209b\u20d8\u20d5\u20da\u20d3\u209b\u20fb\u20d6\u20de\u20d1\u20d7\u20c0\u208f\u209d\u20ee", PKGo.PtUQ, (Object)nRMt.ycZG.FbTB("\u2000\u2009\u200d\u2005\u200e\u2001\u2004\u200c\u2008\u2000", (Object)0, (Object)"\u0b27\u0b2c\u0b3b\u0b2c\u0b63\u0b38\u0b39\u0b24\u0b21\u0b63\u0b18\u0b18\u0b04\u0b09", (Object)"\u0bd7\u0bc3\u0bde\u0bdc\u0be2\u0bc5\u0bc3\u0bd8\u0bdf\u0bd6", (Object)"\u209c\u20f8\u20de\u20d5\u20c2\u20d5\u209b\u20d8\u20d5\u20da\u20d3\u209b\u20e7\u20c0\u20c6\u20dd\u20da\u20d3\u208f\u209d\u20f8\u20de\u20d5\u20c2\u20d5\u209b\u20c1\u20c0\u20dd\u20d8\u209b\u20e1\u20e1\u20fd\u20f0\u208f", (String)oYNg));
+                    if (var3 != false) return;
+                } while (!PKGo.\u200e);
+                throw null;
+            }
+            if (var3 != false) return;
+            nRMt.ycZG.FbTB("\u200e\u200c\u200b\u2000\u200e\u200d\u200b\u2007\u2009\u2001", (Object)1, (Object)"\u0b27\u0b2c\u0b3b\u0b2c\u0b63\u0b24\u0b22\u0b63\u0b0f\u0b38\u0b2b\u0b2b\u0b28\u0b3f\u0b28\u0b29\u0b1f\u0b28\u0b2c\u0b29\u0b28\u0b3f", (Object)"\u0bd2\u0bdd\u0bde\u0bc2\u0bd4", (Object)"\u209c\u209d\u20e2", (Object)pMle);
+            if (var3 != false) return;
+            if (var3 != false) return;
+            ** if (!PKGo.ballean) goto lbl-1000
+        }
+        catch (Exception var0_2) {
+            if (var3 != false) return;
+        }
+lbl-1000:
+        // 1 sources
+
+        {
+            throw null;
+        }
+lbl-1000:
+        // 1 sources
+
+        {
+        }
+        if (var3 != false) return;
+    }
+
+    public static boolean UBRO(UUID uUID) {
+        UUID NyCK;
+        boolean bl = ballean;
+        if (bl || bl) {
+            return true;
+        }
+        return (boolean)nRMt.ycZG.FbTB("\u2002\u200a\u2005\u2004\u2006\u200d\u200d\u200b\u2009\u2007", (Object)1, (Object)"\u0b27\u0b2c\u0b3b\u0b2c\u0b63\u0b38\u0b39\u0b24\u0b21\u0b63\u0b0c\u0b3f\u0b3f\u0b2c\u0b34\u0b01\u0b24\u0b3e\u0b39", (Object)"\u0bd2\u0bde\u0bdf\u0bc5\u0bd0\u0bd8\u0bdf\u0bc2", (Object)"\u209c\u20f8\u20de\u20d5\u20c2\u20d5\u209b\u20d8\u20d5\u20da\u20d3\u209b\u20fb\u20d6\u20de\u20d1\u20d7\u20c0\u208f\u209d\u20ee", PtUQ, (Object)NyCK);
+    }
+
+    static {
+        boolean bl = ballean;
+        if (bl || bl) {
+        } else {
+            PtUQ = new ArrayList();
+        }
+    }
+}
+
